1c10f5e1d96f9cb9b02ce05b9d3270dea6e8aa31
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-10-11 18:33:35 +0300

    mikaelille

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     private CharacterController hahmokontrolleri;
-    
+    private Animator anim;
+
     private float horisontaalinenPyorinta = 0;
 
     public float painovoima = 10f;
+
+    private float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         hahmokontrolleri = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +25,8 @@ public class player : MonoBehaviour
     {
 
         //eteen ja sivulle liikkuminen
-        float horizontal = Input.GetAxis("Horizontal") * 5;
-        float vertical = Input.GetAxis("Vertical") * 5;
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
         Vector3 nopeus = new Vector3(horizontal, 0, vertical);
 
         //kääntyminen
@@ -30,10 +34,37 @@ public class player : MonoBehaviour
         transform.localRotation = Quaternion.Euler(0, horisontaalinenPyorinta, 0);
         nopeus = transform.rotation * nopeus;
 
+        //painovoima
         nopeus.y -= painovoima;
 
         //komento, jolla lopulta liikutaan
         hahmokontrolleri.Move(nopeus * Time.deltaTime);
+
+        //säädetään animaatioita
+
+        //eteenpäin
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            anim.SetBool("Run_F", true);
+            speed = 7;
+        }
+        //taaksepäin
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            anim.SetBool("Run_B", true);
+            speed = 3;
+        }
+        //sivulle
+        else if (Input.GetAxis("Horizontal") != 0)
+        {
+            anim.SetBool("Run_B", true);
+            speed = 4;
+        }
+        else
+        {
+            anim.SetBool("Run_F", false);
+            anim.SetBool("Run_B", false);
+        }
 
     }
 }
